e2248fbf938d5e770fd70581c298cc57131d1eb5
23129ps <23129ps@hvhs.school.nz>
2025-05-12 21:54:57 +0000

    started part 1

---
+++ part_1.js
@@ -0,0 +1,40 @@
+/********************
+Part 1 project
+Written by Priya Silwal
+ Written in 13/05/2025
+ Purpose: Part 1 project
+ *******************/
+alert("Welcome to Rob’s rentals!");
+
+ //Variables//
+userName = prompt("What is your name?");
+alert("Hi " + userName+".");
+let days = ["1,2,3,4,5,6,7,8,9,10"];
+let cost =0;
+let choice= prompt("Hi " + userName +"." n "How many seats do you need in your car?");
+let array =["BAC Mono, MX5, Mini,Falcon, Outlander, Estima"];
+
+
+
+ /******* function askUserAge () {
+    userAge = Number(prompt("What is your age?"));
+ }
+
+ function askUserMoney  ()  {
+    money = Number(prompt("How much pocket money they have?"));
+    alert("You have "+ money+ " dollars."); 
+  if (money >= 20){
+    alert("You can afford a chocolate bar");
+  }
+ else {
+    alert("You can't afford a chocolate bar");
+ }
+ }
+
+/**********
+  Main Code
+***********/
+
+ 
+
+ 
