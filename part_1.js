7a187f4fbba046515c7555ed03972f78c8fd7f47
23129ps <23129ps@hvhs.school.nz>
2025-05-14 00:29:38 +0000

    corrected errors

--- part_1.js
+++ part_1.js
@@ -8,21 +8,20 @@ Written by Priya Silwal
 let userName;
 let cost =0;
 let days = ["1,2,3,4,5,6,7,8,9,10"];
-let array =["BAC Mono, MX5, Mini,Falcon, Outlander, Estima"];
+let array =["BAC Mono",  "MX5" , "Mini", "Falcon", "Outlander", "Estima"];
 
  function start () {
-    userName = askUserName();
-    seats();
+    rental();
  }
-
-  function askUserName ()  {
+  function rental ()  {
 alert("Welcome to Rob’s rentals!");
-  userName = prompt("What is your name?");
+   userName = prompt("What is your name?");
+  let array =["BAC Mono",  "MX5" , "Mini", "Falcon", "Outlander", "Estima"];
+  let choice = prompt("Hi " + userName + ".\nHow many seats do you need in your car?");
+  alert( "I think you might be interested in buying " + array[choice]);
+   days = prompt("For how long do you want to rent the car?");
+   prompt("You have rented " + array[choice] + " for " + days + "days\n Your total cost is" + days * choice);
  }
 
- function seats () {
-  let array =["BAC Mono, MX5, Mini,Falcon, Outlander, Estima"];
-let choice = prompt("Hi " + userName + ".\nHow many seats do you need in your car?");
-  alert( "I think you might be interested in buying " + array[choice]);
- }
+ 
  
