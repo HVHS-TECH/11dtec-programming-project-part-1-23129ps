a5f88334b34eb2358c151429c18101e1d061b6b7
23129ps <23129ps@hvhs.school.nz>
2025-05-15 00:56:17 +0000

    May 15, 2025, 12:56 AM

--- part_1.js
+++ part_1.js
@@ -7,7 +7,7 @@ Written by Priya Silwal
 
 let userName;
 let cost =0;
-let days = ["1,2,3,4,5,6,7,8,9,10"];
+let days;
 let array =["N/A", "BAC Mono",  "MX5" , "Mini", "Falcon", "Outlander", "Estima"];
 
  function start () {
@@ -16,11 +16,16 @@ let array =["N/A", "BAC Mono",  "MX5" , "Mini", "Falcon", "Outlander", "Estima"]
   function rental ()  {
 alert("Welcome to Rob’s rentals!");
    userName = prompt("What is your name?");
-  let array =["BAC Mono",  "MX5" , "Mini", "Falcon", "Outlander", "Estima"];
+  let array =["N/A", "BAC Mono",  "MX5" , "Mini", "Falcon", "Outlander", "Estima"];
   let choice = prompt("Hi " + userName + ".\nHow many seats do you need in your car?");
-  alert( "I think you might be interested in buying " + array[choice]);
+  alert( "I think you might be interested in buying " + array[choice] + ".");
    days = prompt("For how long do you want to rent the car?");
-   prompt("You have rented " + array[choice] + " for " + days + " days\n Your total cost is" + days * choice);
+   if days = 1{
+      prompt("You have rented " + array[choice] + " for " + days + " day.\n Your total cost is $" + days * choice + ".");
+   }
+   else {
+      prompt("You have rented " + array[choice] + " for " + days + " days.\n Your total cost is $" + days * choice + ".");
+   }
  }
 
  
