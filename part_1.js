97ff544e428128986d49240ce83a4e2ac30c2dd5
23129ps <23129ps@hvhs.school.nz>
2025-05-16 03:12:09 +0000

    functions added

--- part_1.js
+++ part_1.js
@@ -8,30 +8,35 @@ Written by Priya Silwal
 //variables///
 
 let userName;
-let cost =0;
-let days;
+let rentDays;
+let choice;
 let array =["N/A", "BAC Mono",  "MX5" , "Mini", "Falcon", "Outlander", "Estima"];
 
 //functions///
 
  function start () {
-    rental();
+   greetUser();
+    car();
+    days();
+    details();
  }
 
-  function rental ()  {
-alert("Welcome to Rob’s rentals!");
-   userName = prompt("What is your name?");
-  let array =["N/A", "BAC Mono",  "MX5" , "Mini", "Falcon", "Outlander", "Estima"];
-  let choice = prompt("Hi " + userName + "!\nHow many seats do you need in your car?");
-  alert( "I think you might be interested in buying " + array[choice] + ".");
-   days = prompt("For how long do you want to rent the car?");
-   if (days > 1) {
-      prompt("You have rented " + array[choice] + " for " + days + " days.\n Your total cost is $" + days * choice + ".");
-   }
-   else {
-      prompt("You have rented " + array[choice] + " for " + days + " day.\n Your total cost is $" + days * choice + ".");
-   }
- }
+  function greetUser () {
+   alert("Welcome to Rob’s rentals!");
+   userName = prompt("Before we start, can you please enter your name?");
+  }
+ 
+  function car () {
+  choice = prompt("Hi " + userName + "!\nHow many seats do you need in your car?");
+      alert( "I think you might be interested in buying " + array[choice] + ".");
+  }
+ 
+  function days () {
+   rentalDays = prompt("For how long do you want to rent the car?");
+  }
 
+  function details () {
+   prompt("You have rented " + array[choice] + " for " + days + " day.\n Your total cost is $" + rentDays * choice + ".");
+  }
  
  
