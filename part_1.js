296ec0cdfa3811ae2856147123dc44eff32fa8d5
23129ps <23129ps@hvhs.school.nz>
2025-05-19 08:02:16 +0000

    May 19, 2025, 8:02 AM

--- part_1.js
+++ part_1.js
@@ -14,6 +14,7 @@ let array = ["N/A", "Solo Hoverpod", "Smart Car", "Mazda RX-8", "Toyota Camry", 
 
 //functions///
 
+//Starts the rental process///
  function start () {
    greetUser();
     car();
@@ -21,20 +22,24 @@ let array = ["N/A", "Solo Hoverpod", "Smart Car", "Mazda RX-8", "Toyota Camry", 
     details();
  }
 
+ //Greets the user and collects their names///
   function greetUser () {
    alert("Welcome to Rob’s rentals!");
    userName = prompt("Before we start, can you please enter your name?");
   }
  
+  //Asks for number of seates needed and suggests a car///
   function car () {
   choice = prompt("Hi " + userName + "!\nHow many seats do you need in your car?");
       alert( "I think you might be interested in buying " + array[choice] + ".");
   }
- 
+
+  //Collects rental duration///
   function days () {
    rentalDays = prompt("For how long do you want to rent the car?");
   }
 
+  //Display rental details with cost///
   function details() {
    alert("You have rented " + array[choice] + " for " + rentalDays + " days.\nYour total cost is $" + (rentalDays * choice) + ".");
 }
