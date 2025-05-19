44a46556cfdc4a26a0ef9a27dfd97a55f8956fc1
23129ps <23129ps@hvhs.school.nz>
2025-05-19 08:07:57 +0000

    May 19, 2025, 8:07 AM

--- part_1.js
+++ part_1.js
@@ -5,16 +5,16 @@ Written by Priya Silwal
  Purpose: Part 1 project
  *******************/
 
-//variables///
+//Variables//
 
 let userName;
 let rentalDays;
 let choice;
 let array = ["N/A", "Solo Hoverpod", "Smart Car", "Mazda RX-8", "Toyota Camry", "Honda CR-V", "Chevrolet Suburban"];
 
-//functions///
+//functions//
 
-//Starts the rental process///
+//Starts the rental process//
  function start () {
    greetUser();
     car();
@@ -22,26 +22,26 @@ let array = ["N/A", "Solo Hoverpod", "Smart Car", "Mazda RX-8", "Toyota Camry", 
     details();
  }
 
- //Greets the user and collects their names///
+ //Greets the user and collects their names//
   function greetUser () {
    alert("Welcome to Rob’s rentals!");
    userName = prompt("Before we start, can you please enter your name?");
   }
  
-  //Asks for number of seates needed and suggests a car///
+  //Asks for number of seates needed and suggests a car//
   function car () {
   choice = prompt("Hi " + userName + "!\nHow many seats do you need in your car?");
       alert( "I think you might be interested in buying " + array[choice] + ".");
   }
 
-  //Collects rental duration///
+  //Collects rental duration//
   function days () {
    rentalDays = prompt("For how long do you want to rent the car?");
   }
 
-  //Display rental details with cost///
+  //Display rental details with cost//
   function details() {
-   alert("You have rented " + array[choice] + " for " + rentalDays + " days.\nYour total cost is $" + (rentalDays * choice) + ".");
+   alert("So, "+ userName+" have rented " + array[choice] + " for " + rentalDays + " days.\nYour total cost is $" + (rentalDays * choice) + ".");
 }
  
  
