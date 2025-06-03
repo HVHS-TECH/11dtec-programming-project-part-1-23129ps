2c358c257b770fe52be6a50ef07829974240848e
23129ps <23129ps@hvhs.school.nz>
2025-06-03 23:34:21 +0000

    Jun 3, 2025, 11:34 PM

--- part_1.js
+++ part_1.js
@@ -1,65 +1,55 @@
-///variables//
+/********************
+Part 1 project
+Written by Priya Silwal
+ Written in 13/05/2025
+ Purpose: Part 1 project
+ *******************/
 
-const array = ["N/A", "Solo Hoverpod", "Smart Car", "Mazda RX-8", "Toyota Camry", "Honda CR-V", "Chevrolet Suburban", "Mercedes-Benz Sprinter", "Ford Transit", "Toyota HiAce"];
+
+//Variables//
 
 
 let userName;
 let rentalDays;
-let numberOfSeats;
-let carPrice;
-let insurance;
+let choice;
+let array = ["N/A", "Solo Hoverpod", "Smart Car", "Mazda RX-8", "Toyota Camry", "Honda CR-V", "Chevrolet Suburban"];
+
 
 //functions//
 
-function start() {
-    greetUser();
-    if (userName === null) return;
 
-
-    numberOfSeats = seats();
-    if (numberOfSeats === null) return; 
-
-
-    rentalDays = days();
-     if (rentalDays === null) return;
-
+//Starts the rental process//
+ function start () {
+   greetUser();
+    car();
+    days();
     details();
-}
-   
+ }
+
+
  //Greets the user and collects their names//
   function greetUser () {
    alert("Welcome to Rob’s rentals!");
-   userName = prompt("What's your name?");
-   
-    // Validate name input
-    while (userName === "" || !isNaN(userName)) {
-        userName = prompt("Invalid input. Please enter a valid name.");
+   userName = prompt("Before we start, can you please enter your name?");
   }
-  if (userName == null) {
-    return;
-  }
-}
-
-
-
+ 
   //Asks for number of seates needed and suggests a car//
-function seats () {
-  numberOfSeats = prompt("Hi " + userName + "!\nHow many seats do you need in your car?");
-
-  
-  if (numberOfSeats === null) return;
-
-  while (numberOfSeats === "" || isNaN(numberOfSeats) || numberOfSeats <= 0 || numberOfSeats > 9) {
-    numberOfSeats = prompt("Invalid input. Please enter a valid number between 1 to 9.");
-    if (numberOfSeats === null) return; 
+  function car () {
+  choice = prompt("Hi " + userName + "!\nHow many seats do you need in your car?");
+      alert( "I think you might be interested in buying " + array[choice] + ".");
   }
-}
 
+
+  //Collects rental duration//
   function days () {
    rentalDays = prompt("For how long do you want to rent the car?");
-   while (rentalDays === "" || isNaN(rentalDays) || rentalDays<=0) {
-        rentalDays = prompt("Invalid input. Please enter a valid input.");
   }
-  console.log(userName + numberOfSeats + rentalDays); 
+
+
+  //Display rental details with cost//
+  function details() {
+   alert("So, "+ userName+" have rented " + array[choice] + " for " + rentalDays + " days.\nYour total cost is $" + (rentalDays * choice * 10) + ".");
 }
-  
+ 
+ 
+
